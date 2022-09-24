3a40e4052a20009dfeb73e2d4afa8d7606dba678
hegehog9 <redpwon@gmail.com>
2022-09-24 19:57:06 +0900

    충돌부분 수정

--- Dream/src/main/webapp/js/login.js
+++ Dream/src/main/webapp/js/login.js
@@ -1,30 +1,3 @@
-
-$(document).ready(function(){
-        // 이메일 주소 클릭시 
-         $("input#input_userid").focus((e)=>{
-            $(e.target).css("outline","none");
-            $(e.target).attr("placeholder"," ");
-         });
-         // 이메일 주소 커서 상실시 
-         $("input#input_userid").blur((e)=>{
-            $(e.target).css({"border":"","border-bottom":""});
-            $(e.target).attr("placeholder","예)dream@dream.co.kr");
-         });
-         
-         // 비밀번호 클릭시 
-         $("input#input_passwd").focus((e)=>{
-            $(e.target).css("outline","none");
-            $(e.target).attr("placeholder"," ");
-         });
-         // 비밀번호 커서 상실시 
-         $("input#input_passwd").blur((e)=>{
-            $(e.target).css({"border":"","border-bottom":""});
-         });
-
-
-      });// end of $(document).ready(function(){=======
-getContextPath();
-
 $(document).ready(function() {
 	// 이메일 주소 클릭시 
 	$("input#input_userid").focus((e) => {
@@ -51,7 +24,6 @@ $(document).ready(function() {
 	$("button#login").click(function(e) {
 		goLogin();
 	});
-
 
 });// end of $(document).ready(function(){}
 
