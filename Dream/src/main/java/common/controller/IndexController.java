ec0d20c30677b35b1b202219254a4af4e683866b
tkddns6621 <tkddns6007@gmail.com>
2022-10-13 21:28:33 +0900

    index 컨트롤러 및 더보기 수정

--- Dream/src/main/java/common/controller/IndexController.java
+++ Dream/src/main/java/common/controller/IndexController.java
@@ -11,6 +11,8 @@ public class IndexController extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		super.goBackURL(request);
+		
 		InterProductDAO pdao = new ProductDAO();
 		
 		int totalDiscount = pdao.totalDiscount(); // 할인 상품의 전체개수를 알아온다.
