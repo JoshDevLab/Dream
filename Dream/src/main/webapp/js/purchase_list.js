dac2a48eef1aa279b61ead88eae23d2dda4d143d
hegehog9 <redpwon@gmail.com>
2022-10-14 15:26:17 +0900

    구매내역 상세 푸터 중복 출력 삭제

--- Dream/src/main/webapp/js/purchase_list.js
+++ Dream/src/main/webapp/js/purchase_list.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
 	
 	 $("div#shipping_cnt_left").trigger("click"); 
 	
-	
 	// 화면인식 480px 사이즈 조절
 	const mediaViewContent = window.matchMedia(`(max-width: 480px)`)	
 	//console.log(mediaViewContent);
@@ -28,9 +27,10 @@ $(document).ready(function() {
     mediaViewContent.addEventListener("change", viewChangeHandler);
 	
 	
-	
 	$("span#totalHITCount").hide();
+	// console.log($("span#totalHITCount").val());
 	$("span#countHIT").hide();
+	// console.log($("span#countHIT").val());
 	
 	// callAjax(start); // 맨 처음 8개를 출력
 	
@@ -55,18 +55,12 @@ $(document).ready(function() {
 		, showMonthAfterYear: true //년도 먼저 나오고, 뒤에 월 표시
 		, changeYear: true //콤보박스에서 년 선택 가능
 		, changeMonth: true //콤보박스에서 월 선택 가능    
-		// input 태그 옆 달력 아이콘 비활성화           
-		// ,showOn: "both" //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시  
-		// ,buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" //버튼 이미지 경로
-		// ,buttonImageOnly: true //기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
 		, buttonText: "선택" //버튼에 마우스 갖다 댔을 때 표시되는 텍스트                
 		, yearSuffix: "년" //달력의 년도 부분 뒤에 붙는 텍스트
 		, monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'] //달력의 월 부분 텍스트
 		, monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'] //달력의 월 부분 Tooltip 텍스트
 		, dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'] //달력의 요일 부분 텍스트
 		, dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'] //달력의 요일 부분 Tooltip 텍스트
-		// ,minDate: "-1M" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
-		// ,maxDate: "+1M" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)                    
 	});
 
 
@@ -345,7 +339,6 @@ function callAjax(start) {
 					$("button#btnMoreHIT").text("처음으로");
 					$("span#countHIT").text("0");
 				}
-   				
    				
    			}
    		}, // end of success
